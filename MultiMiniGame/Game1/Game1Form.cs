923938fab9157a12ccdda24b361dc01d9bc69cd1
Horn Sereyboth <197561773+Sokliang@users.noreply.github.com>
2026-01-28 15:48:46 +0700

    Add some game logic

--- MultiMiniGame/Game1/Game1Form.cs
+++ MultiMiniGame/Game1/Game1Form.cs
@@ -10,10 +10,17 @@ namespace MultiMiniGame.Game1
 {
     public partial class Game1Form : Form
     {
+        Random _random = new Random();
+        string[] words = { "Apple", "Banana", "Orange", "Mango", "Grape", "Lemon",
+    "Dog","Cat","Cow","Pig","Fish","Bird","Duck","Horse","Rabbit","Chicken",
+    "Pineapple","Watermelon","Papaya", "Cloud","Rock","Leaf","Water", "Strawberry",
+    "Chair","Table","Bed","Sofa","Door","Window","Lamp","Fan","Pillow","Mirror",
+    "Book","Pen","Pencil","Bag","Ruler","Eraser","Clock","Phone","Bottle","Cup",
+    "Sun","Moon","Star","Tree","Flower","Rain" };
         string prompt = @"Give ONE short hint (3–6 words) for a word guessing game. You give a hint. The player guesses the word. 
 Do NOT define, classify, or give the word. 
 Avoid repeating previous hints 
-The Word is: orange 
+The Word is: Shark
 Hint:";
         private AIManager _ai = new AIManager();
         public Game1Form()
@@ -36,22 +43,25 @@ Hint:";
 
         private async void btnStart_Click(object sender, EventArgs e)
         {
+            btnSend.Enabled = true;
+            string rndWord = words[_random.Next(words.Length)];
+            string temPrompt = prompt.Replace("Shark", rndWord);
+
             _ai.ResetMemory();
             rtbChat.Clear();
 
-            string response = await _ai.GetResponseAsync(prompt);
+            string response = await _ai.GetResponseAsync(temPrompt);
             AddMessage("Hint: ", response);
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
         private void AddMessage(string sender, string msg)
         {
-            rtbChat.AppendText(sender + ": ");
+            rtbChat.AppendText(sender + " ");
 
-            // Make the message Regular
             rtbChat.SelectionFont = new Font(rtbChat.Font, FontStyle.Regular);
             rtbChat.AppendText(msg + Environment.NewLine + Environment.NewLine);
 
