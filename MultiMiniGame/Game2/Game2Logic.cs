21ac3a8d6269a8b392949c32815108273c096dff
Xakuraii <houngjk081@gmail.com>
2026-02-03 00:06:02 +0700

    save game

--- MultiMiniGame/Game2/Game2Logic.cs
+++ MultiMiniGame/Game2/Game2Logic.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.Json;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
 
 namespace MultiMiniGame.Game2
